03a895a4ccda4015211b63a5811ca828dca13368
Roman Samarev <rssdev10@gmail.com>
2023-03-13 18:26:08 +0300

    added @spawn-function

--- sleep_test.jl
+++ sleep_test.jl
@@ -14,13 +14,31 @@ function test_runner(func, titles, n)
     @info log("Done")
 end
 
+function test_spawn_runner(func, titles, n)
+    @info log("Running tasks for $titles")
+    @sync for _ in 1:n
+        Threads.@spawn func()
+    end
+    @info log("Done")
+end
+
+
 N = 10
 @info log("Actual threads: $(Threads.nthreads()), Requested tasks: $N")
 
-# test 1. Pure Julia sleep()
-@time test_runner(() -> sleep(1), "Julia", N)
-
-# test 2. PyCall
 using PyCall
 tm = pyimport("time")
-@time test_runner(() -> tm.sleep(1), "PyCall", N)
+
+
+for func in [
+    test_runner,
+    test_spawn_runner
+]
+    @info string("Testing:", func)
+
+    # test 1. Pure Julia sleep()
+    @time func(() -> sleep(1), "Julia", N)
+
+    # test 2. PyCall
+    @time func(() -> tm.sleep(1), "PyCall", N)
+end
